90af9e846b06f0b6dcb7007ed5c5aaffbb1fa8c7
Chien Hoang <hoangducchien97@gmail.com>
2019-09-20 15:54:16 +0700

    create component long nhau

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <!-- Goi component Header -->
     <Header></Header>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> 
+    <ListUser></ListUser>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>  -->
     <!-- msg la prop cua component HelloWorld -->
     <Footer></Footer>
     
@@ -14,13 +14,15 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ListUser from './components/ListUser'
 export default {
   name: 'app',
   // Khai bao cac component
   components: {
     HelloWorld,
     Header,
-    Footer
+    Footer,
+    ListUser
   }
 }
 </script>
---
+++ src/components/ListUser.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="list-user">
+        <!-- <p>List User Component</p> -->
+        <UserDetail/>
+        <UserDetail/>
+        <UserDetail/>
+        <UserDetail/>
+    </div>
+</template>
+
+<script>
+import UserDetail from './UserDetail'
+export default {
+    name: 'list-user',
+    data(){
+        return {
+            
+        }
+    },
+    components:{
+        UserDetail
+    }
+}
+</script>
+
+<style>
+
+</style>
